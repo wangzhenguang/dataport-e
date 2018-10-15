d37a21578c77c518a61f949be8a092d7bcdb9206
czy <czysong@qq.com>
2018-10-15 16:18:33 +0800

    hdfs测试

---
+++ hdfs/src/HdfsTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class HdfsTest {
+
+
+    @Before
+    public void before() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
